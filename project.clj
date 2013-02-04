a3f1e66c3a50c31609fc294b8518b943557e0384
Bronsa <brobronsa@gmail.com>
2013-02-04 14:33:40 +0100

    blind is now clojure.tools.reader

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject bronsa/blind "0.5.0"
-  :description "A clojure reader written in clojure"
-  :url "https://github.com/Bronsa/blind"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
